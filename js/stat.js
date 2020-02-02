594dc0d48d0ad95ff758970bffd416db2a0f55ad
Ibrahim <antonutkin777@gmail.com>
2020-02-02 21:33:55 +0300

    Добавил к учебному проекту stat.js

---
+++ js/stat.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 110;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 50;
+var COLUMN_WIDTH = 40;
+var COLUMN_X = 150;
+var COLUMN_Y = 100;
+var COLUMN_GAP = FONT_GAP + COLUMN_WIDTH;
+var NAMES_Y = 270;
+var NAMES_COLOR = 'rgba(0, 0, 0, 1)';
+var columnMaxHeigth = 150;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  
+  return maxElement;
+};
+
+window.renderStatistics = (function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  var maxTime = getMaxElement(times);
+
+  ctx.font = "16px PT Mono";
+  ctx.fillStyle = NAMES_COLOR;
+  ctx.fillText('Ура вы победили!', 130, 40);
+  ctx.fillText('Список результатов:', 130, 60);  
+
+  for (var i = 0; i < names.length; i++) {
+    if (names [i] == 'Вы') {
+      var columnColor = 'rgba(255, 0, 0, 1)';
+    } else {
+      var columnColor = 'rgba(2,14,134, 1)';
+    }        
+
+    ctx.fillStyle = NAMES_COLOR;
+    ctx.fillText(names [i], COLUMN_X + COLUMN_GAP * i, NAMES_Y);
+    ctx.fillStyle = columnColor;
+    ctx.fillRect(COLUMN_X + COLUMN_GAP * i, COLUMN_Y, COLUMN_WIDTH, (maxTime * columnMaxHeigth)/ times[i]);
+  }
+});
